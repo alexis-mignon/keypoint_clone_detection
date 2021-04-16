d1625b69117769b29f4763518ac6f75e436c5cc8
Alexis Mignon <alexis.mignon@gmail.com>
2021-04-17 01:22:22 +0200

    Passed all the algorithms to opencv + SIFT + FLANN

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import keypoint_clone_detection as kcd
 @st.cache(allow_output_mutation=True)
 def load_image(uploaded_image, image_dir=None):
     img = imread(uploaded_image)
+    img = kcd.check_image(img)
     if image_dir is not None:
         if not os.path.exists(image_dir):
             raise RuntimeError("Image save directory does not exist")
@@ -21,17 +22,33 @@ def load_image(uploaded_image, image_dir=None):
 
 
 @st.cache(allow_output_mutation=True)
-def compute_matches(img):
-    img_matches = kcd.extract_keypoints_and_draw_matches(
-        img
+def equalize(img):
+    return kcd.equalize(img)
+
+
+@st.cache(allow_output_mutation=True)
+def compute_matches(img_eq, n_keypoints):
+    kpts, descr = kcd.extract_sift_descr(img_eq, n_keypoints=n_keypoints)
+
+    matches = kcd.find_matches(descr)
+    return (
+        kcd.convert_keypoints_descriptors(kpts, descr),
+        kcd.convert_matches(matches)
     )
-    return img_matches
+
+
+@st.cache()
+def draw_matches(img, keypoints, matches, threshold, damp_factor=5.0, color="jet"):
+    return kcd.draw_matches(img, keypoints, matches,
+                            color=color, damp_factor=damp_factor, threshold=threshold)
+
 
 @st.cache()
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--save-image-dir", default=None, help="Image to save uploaded images in.")
     return parser.parse_args()
+
 
 args = parse_args()
 
@@ -41,13 +58,42 @@ uploaded_image = st.file_uploader("Choose an image file")
 
 if uploaded_image is not None:
     img = load_image(uploaded_image, args.save_image_dir)
-    img_matches = compute_matches(img)
+    img_eq = equalize(img)
 
-    show_matches = st.checkbox(label="show matches", value=True)
-    if show_matches:
-        st.image(img_matches)
-    else:
-        st.image(img)
+    n_keypoints = st.number_input("Number of keypoints:", value=2000)
+
+    keypoints, matches = compute_matches(img_eq, n_keypoints=n_keypoints)
+
+    st.markdown(f"Number of detected keypoints: {keypoints.n_keypoints}")
+    st.markdown(f"Number of found matches: {matches.n_matches}")
+
+    show_matches = st.checkbox(label="show matches", value=True,
+                               help="Draw a line between matching keypoints")
+
+    show_equal = st.checkbox(label="show equalized image ", value=False,
+                             help="Show the image with enhanced local contrast")
+
+    threshold = st.slider(label="Descriptor distance threshold (in quantile): ",
+                          min_value=0.0, max_value=1.0, value=1.0, step=0.01)
+
+    damp = st.number_input("Opacity damping factor:", value=5.0, step=0.1,
+                           help=(
+                               "Set the opacity as a decreasing function of the "
+                               "descriptor distance. A damp factor 0.0 means that "
+                               "all lines have a full opacity. Higher values make "
+                               "the opacity drop faster."
+                           ))
+
+    img_ = img_eq if show_equal else img
+    img_matches = draw_matches(
+        img_,
+        keypoints,
+        matches,
+        threshold=threshold,
+        damp_factor=damp
+    )
+
+    st.image(img_matches if show_matches else img_)
 
 
 st.sidebar.markdown("# Key point matching")
--- keypoint_clone_detection.py
+++ keypoint_clone_detection.py
@@ -1,7 +1,7 @@
 import argparse
 import collections
 
-from scipy.spatial import cKDTree
+from matplotlib import cm
 import numpy as np
 import cv2
 from skimage import draw
@@ -9,10 +9,24 @@ from skimage.color import color_dict
 
 __version__ = "0.0.1"
 
+FLANN_INDEX_KDTREE = 1
 
-Descriptor = collections.namedtuple(
-    "Descriptor", ["keypoints", "descriptors", "orientations", "scales"]
+
+KeyPoints = collections.namedtuple(
+    "KeyPoints", ["n_keypoints", "locations", "descriptors", "orientations", "scales"]
 )
+
+Matches = collections.namedtuple(
+    "Matches", ["n_matches", "train_indexes", "query_indexes", "distances"]
+)
+
+
+def check_image(img):
+    if img.ndim == 3 and img.shape[2] == 4:
+        img = img[..., :3]
+    if img.ndim == 2:
+        img = np.dstack([img] * 3)
+    return img
 
 
 def equalize(img, radius=None):
@@ -25,7 +39,7 @@ def equalize(img, radius=None):
     return clahe.apply(img_gr)
 
 
-def extract_sift_descr(img, n_keypoints=None):
+def extract_sift_descr(img, n_keypoints):
     """ Extract orb descriptors
 
     Parameters
@@ -37,33 +51,29 @@ def extract_sift_descr(img, n_keypoints=None):
 
     Returns
     -------
-    descr: Descriptor
+    kpts: list of cv2.Keypoint
+        The list of detected keypoints
+
+    descr: list of
         Object holding the keypoints and descriptors
     """
-    max_size = max(*img.shape[:2])
-
-    if n_keypoints is None:
-        n_keypoints = max_size
-
-    img_eq = equalize(img)
-
     sift = cv2.SIFT_create(nfeatures=n_keypoints)
-    kpts = sift.detect(img_eq)
-    kpts, descriptors = sift.compute(img, kpts)
-
-    keypoints = np.array([kp.pt[::-1] for kp in kpts])
-
-    descr = Descriptor(
-        keypoints=keypoints,
-        descriptors=descriptors,
-        orientations=np.array([kp.angle for kp in kpts]),
-        scales=np.array([kp.size for kp in kpts])
-    )
-
-    return descr
+    return sift.detectAndCompute(img, mask=None)
 
 
-def find_matches(descriptors):
+def drop_duplicate_matches(matches):
+    indexes = set()
+    dedup_matches = []
+    for m in matches:
+        i, j = m.queryIdx, m.trainIdx
+        ij = (i, j) if i < j else (j, i)
+        if ij not in indexes:
+            indexes.add(ij)
+            dedup_matches.append(m)
+    return dedup_matches
+
+
+def find_matches(descriptors, filter_matches=False):
     """Match descriptors to themselves
 
     Parameters
@@ -73,14 +83,46 @@ def find_matches(descriptors):
 
     Returns
     -------
-    ineighb: array of int, shape=[n_keypoints]
-        The indices of the neighbor of each input point
-    neighb_dist: array of flaot, shape=[n_keypoints]
-        The distance to the nearest neighbor
+    matches: list of cv2.Match
+        The found matches
     """
-    kdtree = cKDTree(descriptors)
-    dists, indices = kdtree.query(descriptors, k=2)
-    return indices.T[1], dists.T[1]
+    print(f"Number of descriptors: {len(descriptors)}")
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    if filter_matches:
+        matches = flann.knnMatch(descriptors, descriptors, k=3)
+        matches = [m1 for _, m1, m2 in matches if m1.distance < 0.7 * m2.distance]
+    else:
+        matches = flann.knnMatch(descriptors, descriptors, k=2)
+        matches = [m for _, m in matches]
+    print(len(matches))
+    matches = drop_duplicate_matches(matches)
+    print(len(matches))
+
+    return matches
+
+
+def convert_keypoints_descriptors(keypoints, descriptors):
+    locations = np.array([kpt.pt[::-1] for kpt in keypoints])
+    angles = np.array([kpt.angle for kpt in keypoints])
+    sizes = np.array([kpt.size for kpt in keypoints])
+    return KeyPoints(
+        n_keypoints=len(keypoints),
+        locations=locations,
+        descriptors=descriptors,
+        orientations=angles,
+        scales=sizes,
+    )
+
+
+def convert_matches(matches):
+    return Matches(
+        n_matches=len(matches),
+        train_indexes=np.array([m.trainIdx for m in matches]),
+        query_indexes=np.array([m.queryIdx for m in matches]),
+        distances=np.array([m.distance for m in matches])
+    )
 
 
 def compute_weights(distances, damp_factor=5.0):
@@ -102,19 +144,38 @@ def compute_weights(distances, damp_factor=5.0):
     return np.exp(- damp_factor * (dists / np.median(dists)) ** 2)
 
 
-def draw_matches(img, descr, color="lime", damp_factor=5.0):
+def get_colors(name, n):
+    cmap = cm.get_cmap(name)
+    if hasattr(cmap, "colors"): # Discrete
+        c = cmap(np.arange(n) % len(cmap.colors))
+    else:
+        c = cmap(np.linspace(0, 1, n))
+    return (c[:, :3] * 255).clip(0, 255).astype("uint8")
+
+
+def draw_matches(img,
+                 keypoints,
+                 matches,
+                 color="lime",
+                 damp_factor=5.0,
+                 threshold=1.0):
     """Add lines joining matching keypoints over the image.
 
     Parameters
     ----------
-    img: array like, shape=[height, width, 3]
+    img: numpy.array, shape=[height, width, 3]
         The input image.
-    descr: Descriptor
+    keypoints: KeyPoints
         The result of the keypoints extraction
+    matches: Matches
+        The result of the keypoint matching
     color: str
         The name of the color (from the `skimage.color.color_dict` dictionary).
     damp_factor: float
         The damp factor used in the gaussian weight profile.
+    threshold: float, optional (default=1)
+        The threshold on descriptor distance above which the matches are displayed expressed
+        in quantile. By default all matches are displayed.
 
     Returns
     -------
@@ -124,17 +185,36 @@ def draw_matches(img, descr, color="lime", damp_factor=5.0):
     """
     try:
         color = (np.array(color_dict[color]) * 255).clip(0, 255).astype("uint8")
+        colors = np.array([color] * matches.n_matches)
     except KeyError:
-        raise ValueError(f"{color!r} is not a valid color name.")
+        try:
+            colors = get_colors(color, matches.n_matches)
+            ishuffle = np.random.permutation(len(colors))
+            colors = colors[ishuffle]
+        except ValueError:
+            raise ValueError(f"{color!r} is not a valid color name or color map name.")
 
-    ineighb, neighb_dist = find_matches(descr.descriptors)
-    keypoints = descr.keypoints.astype("int")
-    weights = compute_weights(neighb_dist, damp_factor=damp_factor)
+    weights = compute_weights(matches.distances, damp_factor=damp_factor)
+
+    if threshold < 1.0:
+        thresh_dist = np.quantile(matches.distances, threshold)
+        mask = matches.distances < thresh_dist
+        train_indexes = matches.train_indexes[mask]
+        query_indexes = matches.query_indexes[mask]
+        weights = weights[mask]
+        colors = colors[mask]
+    else:
+        train_indexes = matches.train_indexes
+        query_indexes = matches.query_indexes
 
     img_draw = img.copy()
+    if img_draw.ndim == 2:
+        img_draw = np.dstack([img_draw] * 3)
 
-    for i, (j, w) in enumerate(zip(ineighb, weights)):
-        (r0, c0), (r1, c1) = keypoints[[i, j]]
+    locations = np.round(keypoints.locations).astype("int")
+
+    for i, j, w, color in zip(train_indexes, query_indexes, weights, colors):
+        (r0, c0), (r1, c1) = locations[[i, j]]
         li, lj, la = draw.line_aa(r0, c0, r1, c1)
         img_draw[li, lj] = (1 - w) * img_draw[li, lj] + w * la[:, None] * color
 
@@ -144,9 +224,15 @@ def draw_matches(img, descr, color="lime", damp_factor=5.0):
 def extract_keypoints_and_draw_matches(img,
                                        n_keypoints=None,
                                        color="lime",
-                                       damp_factor=5.0):
-    descr = extract_sift_descr(img, n_keypoints=n_keypoints)
-    return draw_matches(img, descr, color=color, damp_factor=damp_factor)
+                                       damp_factor=5.0,
+                                       threshold=1.0):
+    kpts, descr = extract_sift_descr(img, n_keypoints=n_keypoints)
+    matches = find_matches(descr)
+
+    keypoints = convert_keypoints_descriptors(kpts, descr)
+    matches = convert_matches(matches)
+
+    return draw_matches(img, keypoints, matches, color=color, damp_factor=damp_factor, threshold=threshold)
 
 
 if __name__ == "__main__":
